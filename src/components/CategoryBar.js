67417a25b43a8120c70c4397a66ceb0bec9b749a
Tuhin Das <mailme.tuhindas@gmail.com>
2021-04-30 00:47:00 +0530

    feat: add navbar, category bar

--- src/components/CategoryBar.js
+++ src/components/CategoryBar.js
@@ -1,10 +1,33 @@
 import "../assests/css/categorybar.css";
+import { NavLink } from "react-router-dom";
+
 const CategoryBar = () => {
   return (
-    <div className="categories">
-      <div className="category">All</div>
-      <div className="category">Tagore Songs</div>
-      <div className="category">Guitar Tutorials</div>
+    <div className="categories--container">
+      <NavLink
+        to="/"
+        className="link category-item"
+        activeClassName="category__active"
+        end
+      >
+        All
+      </NavLink>
+      <NavLink
+        to="/categories/tagore-songs"
+        className="link category-item"
+        activeClassName="category__active"
+        end
+      >
+        Tagore Songs
+      </NavLink>
+      <NavLink
+        to="/categories/guitar-tutorials"
+        className="link category-item"
+        activeClassName="category__active"
+        end
+      >
+        Guitar Tutorials
+      </NavLink>
     </div>
   );
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,38 @@
 import { useRef } from "react";
+import { Link } from "react-router-dom";
 import "../assests/css/navbar.css";
-import useOnScrollNavbarAnimation from "../hooks/useOnScrollNavbarAnimation";
+import { useSearch } from "../contexts/SearchProvider";
 const NavBar = () => {
-  const scrollRef = useRef(null);
-  useOnScrollNavbarAnimation(scrollRef);
-  return (
-    <nav ref={scrollRef} className="navbar">
-      <div className="nav-left">
-        <div className="nav-brand">
-          <h1>RabiTube</h1>
-        </div>
-      </div>
-      <div className="nav-right nav-right--large">
-        <i className="bi bi-search"></i>
-        <i className="bi bi-person-circle"></i>
-      </div>
-    </nav>
-  );
+	const scrollRef = useRef(null);
+	const { searchInput, setSearchInput } = useSearch();
+
+	return (
+		<nav ref={scrollRef} className="navbar">
+			<Link to="/" className="nav-link">
+				<div className="nav-left">
+					<div className="nav-brand">
+						<h1>RabiTube</h1>
+					</div>
+				</div>
+			</Link>
+
+			<form id="search-form" className="search-desktop" action="">
+				<input
+					value={searchInput}
+					onChange={(e) => setSearchInput(e.target.value)}
+					className="search-box"
+					type="text"
+				/>
+				<button className="search-button">
+					<i className="bi bi-search"></i>
+				</button>
+			</form>
+
+			<div className="nav-right nav-right--large">
+				<i className="bi bi-search search-mobile"></i>
+				<i className="bi bi-person-circle"></i>
+			</div>
+		</nav>
+	);
 };
 export default NavBar;
