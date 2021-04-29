b5e56b70749974bce1e679e11d1fed00c656b637
Tuhin Das <mailme.tuhindas@gmail.com>
2021-04-30 00:48:17 +0530

    feat: add video listing

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,28 +1,57 @@
 import "../assests/css/videolistitem.css";
+import { Link } from "react-router-dom";
 import generateThumbnail from "../utils/generateThumbnail";
-const VideoListItem = ({ id, avatar, title, views, channel, postedOn }) => {
+
+const VideoListItem = ({
+  v_id,
+  avatar,
+  title,
+  views,
+  channel,
+  postedOn,
+  onOptionClick,
+}) => {
+  const handleShowModal = () => {
+    if (typeof onOptionClick === "function")
+      onOptionClick({
+        v_id,
+        avatar,
+        title,
+        views,
+        channel,
+        postedOn,
+        onOptionClick,
+      });
+  };
+
   return (
-    <div className="video-item">
-      <img className="image" src={generateThumbnail(id)} alt="" />
-      <div className="video-item--particulars">
-        <div className="particulars--avatar">
-          <div className="avatar">
-            <img className="image round" src={avatar} alt="avatar" />
+    <div className="video--item">
+      <Link className="link" to={`/video/${v_id}`}>
+        <img className="image" src={generateThumbnail(v_id)} alt="thumbnail" />
+      </Link>
+      <div className="item--info-container">
+        <Link className="link" to={`/video/${v_id}`}>
+          <div className="info--avatar">
+            <div className="avatar">
+              <img className="image round" src={avatar} alt="avatar" />
+            </div>
           </div>
-        </div>
-        <div className="particulars--metadata">
-          <p className="metadata--title">
-            <strong>{title}</strong>
-          </p>
-          <p className="metadata--details">
-            {channel}
-            <i className="bi bi-dot"></i>
-            {views}
-            <i className="bi bi-dot"></i>
-            {postedOn}
-          </p>
-        </div>
-        <div className="particulars--three-dot">
+        </Link>
+        <Link className="link" to={`/video/${v_id}`}>
+          <div className="info--detail-container">
+            <p className="detail--title">
+              <strong>{title}</strong>
+            </p>
+            <p className="detail-extra">
+              <span>{channel}</span>
+              <i className="bi bi-dot"></i>
+              <span>{views} views</span>
+              <i className="bi bi-dot"></i>
+              {postedOn}
+            </p>
+          </div>
+        </Link>
+        <div onClick={handleShowModal} className="option-menu--container">
           <i className="bi bi-three-dots-vertical"></i>
         </div>
       </div>
